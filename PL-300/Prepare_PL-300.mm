0b10b0e4f65e3223daa930ab03a9abc6dc06e803
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-04-27 11:51:09 +0800

    initialize PL-300 structure

--- PL-300/Prepare_PL-300.mm
+++ PL-300/Prepare_PL-300.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.12.1">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Prepare_PL-300" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1745722987802">
+<node TEXT="Prepare_PL-300&#xa;(PowerBI Data Analyst)" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1745724840486" LINK="https://learn.microsoft.com/en-us/credentials/certifications/data-analyst-associate/?practice-assessment-type=certification">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <font BOLD="true"/>
 <hook NAME="MapStyle" background="#fdf6e3ff" zoom="0.826">
@@ -87,5 +87,38 @@
 </stylenode>
 </map_styles>
 </hook>
+<node TEXT="1. Get Started with Microsoft Data Analytics" POSITION="bottom_or_right" ID="ID_777219713" CREATED="1745723017943" MODIFIED="1745723031338">
+<node TEXT="1.1 Discover Data Analysis" ID="ID_65818307" CREATED="1745723105311" MODIFIED="1745723118393"/>
+<node TEXT="1.2 Get Started Building with Power BI" ID="ID_442186763" CREATED="1745723118623" MODIFIED="1745723126069"/>
+<node TEXT="1.3 Create Interactive Reports using Copilot for Power BI" ID="ID_1543254417" CREATED="1745723126224" MODIFIED="1745723145458"/>
+</node>
+<node TEXT="2. Prepare Data for Analysis with Power BI" POSITION="bottom_or_right" ID="ID_1352729325" CREATED="1745723031624" MODIFIED="1745723038283">
+<node TEXT="2.1 Get Data in Power BI" ID="ID_868279455" CREATED="1745723165095" MODIFIED="1745723169931"/>
+<node TEXT="2.2 Clean, Transform, and Load Data in Power BI" ID="ID_617950978" CREATED="1745723170080" MODIFIED="1745723184442"/>
+<node TEXT="2.3 Choose a Power BI Model Framework" ID="ID_1982047615" CREATED="1745723184623" MODIFIED="1745723191906"/>
+</node>
+<node TEXT="3. Model Data with Power BI" POSITION="bottom_or_right" ID="ID_1062766974" CREATED="1745723038432" MODIFIED="1745723043196">
+<node TEXT="3.1 Design a Semantic Model in Power BI" ID="ID_561395417" CREATED="1745723202423" MODIFIED="1745723214794"/>
+<node TEXT="3.2 Write DAX Formulas for Power BI Desktop Models" ID="ID_1048206500" CREATED="1745723214951" MODIFIED="1745723229122"/>
+<node TEXT="3.3 Add Measures to Power BI Desktop Models" ID="ID_860530586" CREATED="1745723229264" MODIFIED="1745723239130"/>
+<node TEXT="3.4 Add Calculated Tables and Columns to Power BI Desktop Models" ID="ID_363022564" CREATED="1745723242176" MODIFIED="1745723254178"/>
+<node TEXT="3.5 Modify DAX Filter Context in Power BI Desktop Models" ID="ID_1890202040" CREATED="1745723255479" MODIFIED="1745723270139"/>
+<node TEXT="3.6 Use DAX Time Intelligence Functions in Power BI Desktop Models" ID="ID_1117272782" CREATED="1745724797515" MODIFIED="1745724814693"/>
+<node TEXT="3.7 Optimize a Model for Performance in Power BI" ID="ID_377226281" CREATED="1745724814875" MODIFIED="1745724825654"/>
+</node>
+<node TEXT="4. Build Power BI Visuals and Reports" POSITION="bottom_or_right" ID="ID_1990050192" CREATED="1745723043359" MODIFIED="1745723051291">
+<node TEXT="4.1 Scope Report Design Requirements" ID="ID_726792139" CREATED="1745724852474" MODIFIED="1745724860139"/>
+<node TEXT="4.2 Design Power BI Reports" ID="ID_1489806501" CREATED="1745724864330" MODIFIED="1745724873853"/>
+<node TEXT="4.3 Create Visual Calculations in Power BI Desktop" ID="ID_668202082" CREATED="1745724874073" MODIFIED="1745725403208"/>
+<node TEXT="4.4 Configure Power BI Report Filters" ID="ID_112143087" CREATED="1745725403940" MODIFIED="1745725412008"/>
+<node TEXT="4.5 Enhance Power BI Report Designs for the User Experience" ID="ID_1793394270" CREATED="1745725416029" MODIFIED="1745725429064"/>
+<node TEXT="4.6 Perform Analytics in Power BI" ID="ID_1585363840" CREATED="1745725429813" MODIFIED="1745725437521"/>
+</node>
+<node TEXT="5. Manage Workspaces and Semantics Models in Power BI" POSITION="bottom_or_right" ID="ID_126874048" CREATED="1745723051575" MODIFIED="1745723062929">
+<node TEXT="5.1 Create and Manage Workspaces in Power BI" ID="ID_781316812" CREATED="1745725453148" MODIFIED="1745725464136"/>
+<node TEXT="5.2 Manage Semantic Models in Power BI" ID="ID_1501975969" CREATED="1745725464301" MODIFIED="1745725474049"/>
+<node TEXT="5.3 Create Dashboards in Power BI" ID="ID_1496661252" CREATED="1745725474221" MODIFIED="1745725482536"/>
+<node TEXT="5.4 Implement Row-Level Security" ID="ID_991820239" CREATED="1745725485589" MODIFIED="1745725493119"/>
+</node>
 </node>
 </map>
---
+++ PL-300/README.md
@@ -0,0 +1,18 @@
+# Prepare PL-300 base on Microsoft Learn
+
+PL-300 is the exam for gain Microsoft Certified Power BI Data Analyst Associate, I'd gotten this certification in 2021 and keep refreshing it in past 4 years. This page (course) aims to go through the [content in Microsoft Learn](https://learn.microsoft.com/en-us/credentials/certifications/data-analyst-associate/?practice-assessment-type=certification) site and demo to you the detail knowledge points, supporting you to prepare and pass the exam.
+
+## Structure (MindMap) of the Learning Contents
+
+![mindmap-pl-300-content](img/Prepare_PL-300-Mindmap-L1-L2.jpg)
+
+## Demo Material
+
+The demo videos will be preapre and shared in following site soon:
+
+- YouTube: Edited Videos
+- Udemy: Packaged Course
+
+Keep checking here on the progress, stay tunes.
+
+And, may you good luck for getting this certification!
